85450d2345b14e33acad21349d4bf542a4b60aa2
Clemente Estrada <mirdrack@gmail.com>
2018-04-28 22:25:23 -0500

    Added ChangePassword and Home controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Monica\Http\Controllers;
+
+use Home\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:web');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
