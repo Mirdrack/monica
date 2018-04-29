64ffd7f760766fdfbbe573580db0abb279b87bf3
Clemente Estrada <mirdrack@gmail.com>
2018-04-28 23:17:23 -0500

    Added routes for dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { return redirect('/admin/home'); });
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
+$this->post('login', 'Auth\LoginController@login')->name('auth.login');
+$this->post('logout', 'Auth\LoginController@logout')->name('auth.logout');
+
+// Change Password Routes...
+$this->get('change_password', 'Auth\ChangePasswordController@showChangePasswordForm')->name('auth.change_password');
+$this->patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('auth.password.reset');
+
+Route::group([/*'middleware' => ['auth.web'],*/ 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/home', 'HomeController@index');
+    Route::resource('abilities', 'Admin\AbilitiesController');
+    Route::resource('roles', 'Admin\RolesController');
+    Route::resource('users', 'Admin\UsersController');
 });
