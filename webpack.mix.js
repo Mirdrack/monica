6e724f3873ffbe9381bb619ab6a0c26a620d3105
Clemente Estrada <mirdrack@gmail.com>
2018-05-06 13:22:29 -0500

    Added configuration to copy the asssets used on the frontend by the adminlet template

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-	.copy('node_modules/admin-lte/dist/css/AdminLTE.min.css', 'public/css/adminlte')
-	.copy('node_modules/admin-lte/dist/css/skins/skin-blue.min.css', 'public/css/adminlte/skins')
-	.sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .copy('node_modules/admin-lte/dist/css/AdminLTE.min.css', 'public/css/adminlte')
+    .copy('node_modules/admin-lte/dist/css/skins/skin-blue.min.css', 'public/css/adminlte/skins')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copy('node_modules/admin-lte/dist/js/app.min.js', 'public/js/adminlte')
+    .copy('resources/assets/js/main.js', 'public/js')
+    .copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'public/js')
+    .copy('node_modules/admin-lte/plugins/select2/select2.full.min.js', 'public/js')
+    .copy('node_modules/admin-lte/plugins/select2/select2.min.css', 'public/css')
