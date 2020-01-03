a9ed99a4a464c32115d9e3fbf9a3706d68cad43b
Clemente Estrada <mirdrack@gmail.com>
2020-01-02 20:58:31 -0600

    Removed facade models and added a view for error workflow

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,23 +1,29 @@
 <?php
 namespace Monica\Http\Controllers;
 
-use Home\Http\Requests;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Monica\Models\Tenant;
 
 class HomeController extends Controller
 {
     /**
+     * @param \Monica\Models\Tenant $tenant
+     */
+    public function __construct(Tenant $tenant)
+    {
+        $this->tenant = $tenant;
+    }
+
+    /**
      * Show the application dashboard.
-     *
+     * @param  string $subdomain
      * @return \Illuminate\Http\Response
      */
     public function index($subdomain)
     {
-        $tenant = Tenant::where('subdomain', $subdomain)->first();
+        $tenant = $this->tenant->where('subdomain', $subdomain)->first();
         if ($tenant) {
             return view('home', compact('tenant'));
         }
+        return response()->view('errors.404', [], 404);
     }
 }
