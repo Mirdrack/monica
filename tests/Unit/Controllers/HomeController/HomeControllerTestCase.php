0f46d34b00483aa41d7bc4d7b0cf2815f5069021
Clemente Estrada <mirdrack@gmail.com>
2020-01-02 20:59:38 -0600

    Added test for HomeController

---
+++ tests/Unit/Controllers/HomeController/HomeControllerTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\HomeController;
+
+use Mockery;
+use Tests\TestCase;
+use Monica\Http\Controllers\HomeController;
+
+class HomeControllerTestCase extends TestCase
+{
+    protected $tenant;
+
+    protected $homeController;
+
+    public function setup()
+    {
+        parent::setUp();
+        $this->tenant = Mockery::mock('Monica\Models\Tenant');
+        $this->homeController = new HomeController($this->tenant);
+    }
+}
