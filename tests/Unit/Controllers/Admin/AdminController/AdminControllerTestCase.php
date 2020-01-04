fff26645b35f81a011631ab11adce790f32b9178
Clemente Estrada <mirdrack@gmail.com>
2020-01-04 10:13:40 -0600

    Added tests for index method in AdminController

---
+++ tests/Unit/Controllers/Admin/AdminController/AdminControllerTestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Controllers\Admin\AdminController;
+
+use Mockery;
+use Tests\TestCase;
+use Monica\Http\Controllers\Admin\AdminController;
+
+class AdminControllerTestCase extends TestCase
+{
+    protected $adminController;
+
+    protected $auth;
+
+    protected $gate;
+
+    protected $admin;
+
+    public function setup()
+    {
+        parent::setup();
+
+        $this->auth = Mockery::mock('Illuminate\Auth\AuthManager');
+        $this->gate = Mockery::mock('Illuminate\Contracts\Auth\Access\Gate');
+        $this->admin = Mockery::mock('Monica\Models\Admin');
+
+        $this->auth->shouldReceive('shouldUse')
+            ->andReturn(null);
+
+        $this->adminController = new AdminController(
+            $this->auth,
+            $this->gate,
+            $this->admin
+        );
+    }
+}
