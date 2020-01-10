a882602ab5d12292fc1358835465d81e3b434b7d
Clemente Estrada <mirdrack@gmail.com>
2020-01-09 21:04:37 -0600

    Added tests for edit method in UserController

--- tests/Unit/Controllers/Dashboard/UserController/EditTest.php
+++ tests/Unit/Controllers/Dashboard/UserController/EditTest.php
@@ -6,9 +6,9 @@ use Mockery;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class CreateTest extends UserControllerTestCase
+class EditTest extends UserControllerTestCase
 {
-    public function testSuccessfulRenderCreate()
+    public function testSuccessfulRenderEdit()
     {
         $this->gate->shouldReceive('allows')
             ->andReturn(true);
@@ -16,26 +16,28 @@ class CreateTest extends UserControllerTestCase
             ->once()->andReturnSelf();
         $this->role->shouldReceive('get')->andReturnSelf();
         $this->role->shouldReceive('pluck')->andReturn(['Admin' => 'admin']);
+        $this->user->shouldReceive('findOrFail')->andReturn($this->user);
 
-        $result = $this->userController->create('test-tenant');
+        $result = $this->userController->edit('test-tenant', 1);
 
         $this->assertInstanceOf('Illuminate\View\View', $result);
-        $this->assertEquals('dashboard.users.create', $result->getName());
+        $this->assertEquals('dashboard.users.edit', $result->getName());
+        $this->assertArrayHasKey('user', $result->getData());
         $this->assertArrayHasKey('roles', $result->getData());
         $this->assertArrayHasKey('tenant', $result->getData());
     }
 
-    public function testForbiddenRenderCreate()
+    public function testForbiddenRenderEdit()
     {
         $this->gate->shouldReceive('allows')
             ->andReturn(false);
 
         $this->expectException(HttpException::class);
 
-        $this->userController->create('test');
+        $this->userController->edit('test', 1);
     }
 
-    public function testCreateWithNonExistentTenant()
+    public function testEditWithNonExistentTenant()
     {
         $this->gate->shouldReceive('allows')
             ->andReturn(true);
@@ -46,6 +48,6 @@ class CreateTest extends UserControllerTestCase
 
         $this->expectException(NotFoundHttpException::class);
 
-        $result = $this->userController->create('test');
+        $result = $this->userController->edit('test', 1);
     }
 }
